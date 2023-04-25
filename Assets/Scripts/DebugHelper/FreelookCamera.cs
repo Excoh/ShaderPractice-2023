028cfb43155cde8fdbef6bccdfe2dfb3eadaa79c
Excoh <excel.ortega@gmail.com>
2023-04-24 21:30:51 -0700

    Finish first version of free look camera

--- Assets/Scripts/DebugHelper/FreelookCamera.cs
+++ Assets/Scripts/DebugHelper/FreelookCamera.cs
@@ -13,8 +13,8 @@ namespace FreeLookCamera
 public class FreelookCamera : MonoBehaviour
 {
     // Update is called once per frame
-    public float cameraSpeed = 1.0f;
-    public float rotateCameraSpeed = 1.0f;
+    public float cameraSpeed = 15.0f;
+    public float rotateCameraSpeed = 45.0f;
 
     private Vector3 prevMousePos;
     private Vector3 currentMousePos;
@@ -26,6 +26,10 @@ public class FreelookCamera : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            prevMousePos = Input.mousePosition;
+        }
         if (IsRightClickPressed() && Input.GetKey(KeyCode.W))
         {
             Camera.main.transform.position += transform.forward * Time.deltaTime * cameraSpeed;
@@ -37,31 +41,33 @@ public class FreelookCamera : MonoBehaviour
 
         if (IsRightClickPressed() && Input.GetKey(KeyCode.A))
         {
-            Camera.main.transform.position += transform.right * -1 * Time.deltaTime * cameraSpeed;
+            Camera.main.transform.Translate(Camera.main.transform.right * -1 * Time.deltaTime * cameraSpeed, Space.World);
         }
         else if (IsRightClickPressed() && Input.GetKey(KeyCode.D))
         {
-            Camera.main.transform.position += transform.right * Time.deltaTime * cameraSpeed;
+            Camera.main.transform.Translate(Camera.main.transform.right * Time.deltaTime * cameraSpeed, Space.World);
         }
 
         if (IsRightClickPressed() && Input.GetKey(KeyCode.Q))
         {
-            Camera.main.transform.position += transform.up * -1 * Time.deltaTime * cameraSpeed;
+            Camera.main.transform.position += Vector3.down * Time.deltaTime * cameraSpeed;
         }
         else if (IsRightClickPressed() && Input.GetKey(KeyCode.E))
         {
-            Camera.main.transform.position += transform.up * Time.deltaTime * cameraSpeed;
+            Camera.main.transform.position += Vector3.up * Time.deltaTime * cameraSpeed;
         }
 
         if (IsRightClickPressed())
         {
             currentMousePos = Input.mousePosition;
             Vector3 direction = currentMousePos - prevMousePos;
-            //transform.RotateAround(transform.position, transform.forward, direction.x * Time.deltaTime * rotateCameraSpeed);
-            //transform.RotateAround(transform.position, transform.right, -direction.y * Time.deltaTime * rotateCameraSpeed);
-
+            if (Mathf.Abs(Vector3.Magnitude(direction)) > 0.0f)
+            {
+                Camera.main.transform.Rotate(0, direction.x * Time.deltaTime * rotateCameraSpeed, 0f, Space.World);
+                Camera.main.transform.Rotate(transform.right, -direction.y * Time.deltaTime * rotateCameraSpeed, Space.World);
+            }
+            prevMousePos = currentMousePos;
         }
-        prevMousePos = Input.mousePosition;
         
 
     }
